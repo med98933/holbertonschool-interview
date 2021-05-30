a79ede7e7d04a8ae83efbdc9555bfd853ce081d6
momo98-sys <m.ladhari@outlook.com>
2021-05-30 12:53:40 +0100

    add

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -12,8 +12,8 @@ request(URL, function (err, res, body) {
         if (error) console.log(error);
         id[i].name = JSON.parse(body2).name;
         if (checkdata(id)) {
-          for (let j = 0; j < id.length; j++) {
-            console.log(id[j].name);
+          for (let x = 0; x < id.length; x++) {
+            console.log(id[x].name);
           }
         }
       });
