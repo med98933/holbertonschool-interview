c55c6ddcea7896a8281269b0e9e11c0ee2b016eb
momo98-sys <m.ladhari@outloo.com>
2021-06-09 19:03:11 +0100

    add

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" 
+Script for the Function rotate_2d_matrix 
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """ 
+    Function that rotates the matrix 90 degrees
+     """
+    s = 0
+    l = len(matrix) - 1
+    while s < l:
+        for i in range(s, l):
+            aux = matrix[i][l]
+            matrix[i][l] = matrix[s][i]
+            aux2 = matrix[l][l + s - i]
+            matrix[l][l + s - i] = aux
+            aux = matrix[l + s - i][s]
+            matrix[l + s - i][s] = aux2
+            matrix[s][i] = aux
+        s += 1
+        l -= 1
