74f9278a23ad0c370eebec5f6aa84b52f7432e36
root <root@DESKTOP-QG51Q48.localdomain>
2022-02-05 21:08:37 +0100

    task0

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values
+"""
+
+
+def makeChange(coins, total):
+    """
+    Make change using minimum coins 
+    """
+    coins.sort(reverse=True)
+    c = 0
+    for coin in coins:
+        c += total // coin
+        total = total % coin
+    if total:
+        return -1
+    return c
