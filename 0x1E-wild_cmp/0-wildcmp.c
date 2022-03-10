870a2d3afc2236866944d0a0f5e706d6cbda44a9
med98933 <m.ladhari@outlook.com>
2022-03-10 23:27:00 +0100

    wildcmp

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+* wildcmp - Compares 2 strings
+*
+* identical, otherwise return 0.
+*
+* @s1: 1st string
+*
+* @s2: 2nd string
+
+* Return:  0 otherwise
+*/
+
+int wildcmp(char *s1, char *s2)
+{
+	int x, i;
+
+	if (!*s1)
+	{
+		if (*s2 == '*')
+		{
+			x = wildcmp(s1, s2 + 1);
+			return (x);
+		}
+
+		i = (*s2 == '\0') ? 1 : 0;
+
+		return (i);
+	}
+
+	if (*s2 == '*')
+	{
+		x = wildcmp(s1, s2 + 1);
+		i = wildcmp(s1 + 1, s2);
+		return (x || i);
+	}
+	if (*s1 == *s2)
+	{
+		x = wildcmp(s1 + 1, s2 + 1);
+		return (x);
+	}
+
+	return (0);
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,9 @@
+#ifndef _HOLBERTON_h_
+#define _HOLBERTON_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int wildcmp(char *s1, char *s2);
+
+#endif
