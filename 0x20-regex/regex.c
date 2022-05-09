2d56265b004fb5959679179a20b9108fac20a97a
med98933 <m.ladhari@outlook.com>
2022-05-10 00:17:53 +0100

    regex

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,39 @@
+#include "regex.h"
+#include <stdio.h>
+#include <string.h>
+
+
+
+/**
+ * regex_match - recursively checks whether a given pattern 
+ *
+ * matches a given string
+ *
+ * @str: the string
+ *
+ * @pattern: Pattern to be compared
+ *
+ * Return: 1 in match, 0 otherwise
+ */
+int regex_match(char const *str, char const *pattern)
+{
+		int c1 = 0, c2 = 0;
+
+	if (!str || !pattern)
+		return (0);
+
+	c1 = *str && (*str == *pattern || *pattern == '.');
+	c2 = *(pattern + 1) == '*';
+
+	if (!*str && !c2)
+		return (*pattern ? 0 : 1);
+
+	if (c1 && c2)
+		return (regex_match(str + 1, pattern) || regex_match(str, pattern + 2));
+	else if (c1 && !c2)
+		return (regex_match(str + 1, pattern + 1));
+	else if (c2)
+		return (regex_match(str, pattern + 2));
+
+	return (0);
+}
---
+++ 0x20-regex/regex.h
@@ -0,0 +1,5 @@
+#ifndef _REGEX_h
+#define _REGEX_h
+
+int regex_match(char const *str, char const *pattern);
+#endif
