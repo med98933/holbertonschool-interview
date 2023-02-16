fca03eee2fb0e48e13fc73e3332003e083b740ab
med98933 <m.ladhari@outlook.com>
2023-02-16 21:46:01 +0100

    pep8

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -1,23 +1,24 @@
 import requests
 
+
 def count_words(subreddit, word_list, after=None, counts=None):
     if counts is None:
         counts = {}
-    
+
     url = f'https://www.reddit.com/r/{subreddit}/hot.json'
     headers = {'User-agent': 'Mozilla/5.0'}
     params = {'limit': 100}
-    
+
     if after:
         params['after'] = after
-        
+
     response = requests.get(url, headers=headers, params=params)
-    
+
     if response.status_code == 200:
         data = response.json()['data']
         after = data['after']
         posts = data['children']
-        
+
         for post in posts:
             title = post['data']['title'].lower()
             for word in word_list:
@@ -27,7 +28,7 @@ def count_words(subreddit, word_list, after=None, counts=None):
                         counts[word] += 1
                     else:
                         counts[word] = 1
-        
+
         if after:
             count_words(subreddit, word_list, after=after, counts=counts)
         else:
